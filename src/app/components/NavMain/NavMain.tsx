7a40e2b1f897c9c7e796083296fe8b407749a549
William Lowrimore <wlowrimore@gmail.com>
2024-04-11 20:31:13 -0500

    adds custom dynamic bg to main nav

--- src/app/components/NavMain/NavMain.tsx
+++ src/app/components/NavMain/NavMain.tsx
@@ -1,10 +1,36 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 import Link from "next/link";
 import "./nav-styles.css";
 
 const NavMain: React.FC = () => {
+  const [color, setColor] = useState<boolean>(false);
+
+  useEffect(() => {
+    const changeColor = () => {
+      if (window.scrollY >= 90) {
+        setColor(true);
+      } else {
+        setColor(false);
+      }
+    };
+    window.addEventListener("scroll", changeColor);
+    return () => {
+      window.removeEventListener("scroll", changeColor);
+    };
+  }, []);
+
   return (
     <nav className="hidden md:inline-flex fixed w-full top-0 z-50">
-      <div className="flex bg-[#304454]/50 py-2 border-b border-zinc-200 text-white text-xl justify-around w-full mx-auto 2xl:justify-center 2xl:gap-[12rem]">
+      <div
+        className={
+          color
+            ? "nav-bg bg-[#304454] 2xl:justify-center 2xl:gap-[10rem]"
+            : "nav-bg bg-[#304454]/50 2xl:justify-center 2xl:gap-[10rem]"
+        }
+      >
         <Link href="/">
           <p className="py-1.5 pl-4 btmbrdr">Home</p>
         </Link>
